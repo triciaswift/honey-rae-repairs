0b1219313991d0b99a6ae4fce10f8187adf7ca0f
Tricia Swift <tricia.swift8@gmail.com>
2023-08-28 14:00:32 -0500

    Part 1 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getAllTickets } from "./services/ticketService";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  //! useState() returns an array
+  // useState(must give initial value to stateVariable or it's undefined)
+  const [allTickets, setAllTickets] = useState([]); // [stateVariable, setterFunction]
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false);
+  const [filteredTickets, setFilteredTickets] = useState([]);
+
+  //! useEffect() callback function: what we want to happen, dependency array: when we want it to happen
+  // takes 2 arguments: (() => {}, [])
+  useEffect(() => {
+    getAllTickets().then((ticketsArray) => {
+      setAllTickets(ticketsArray);
+      console.log("tickets set!");
+    });
+  }, []); // ONLY runs on initial render of component
+
+  useEffect(() => {
+    if (showEmergencyOnly) {
+      const emergencyTickets = allTickets.filter(
+        (ticket) => ticket.emergency === true
+      );
+      setFilteredTickets(emergencyTickets);
+    } else {
+      setFilteredTickets(allTickets);
+    }
+  }, [showEmergencyOnly, allTickets]); // runs on initial render & when showEmergencyOnly or allTickets changes
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <div>
+        <button
+          className="filter-btn btn-primary"
+          onClick={() => {
+            setShowEmergencyOnly(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Emergency
+        </button>
+        <button
+          className="filter-btn btn-info"
+          onClick={() => {
+            setShowEmergencyOnly(false);
+          }}
+        >
+          Show All
+        </button>
+      </div>
+      <article className="tickets">
+        {filteredTickets.map((ticket) => {
+          return (
+            //! Each child in a list should have a unique "key" prop
+            <section className="ticket" key={ticket.id}>
+              <header className="ticket-info">#{ticket.id}</header>
+              <div>{ticket.description}</div>
+              <footer>
+                <div>
+                  <div className="ticket-info">Emergency</div>
+                  <div>{ticket.emergency ? "yes" : "no"}</div>
+                </div>
+              </footer>
+            </section>
+          );
+        })}
+      </article>
     </div>
   );
-}
-
-export default App;
+};
